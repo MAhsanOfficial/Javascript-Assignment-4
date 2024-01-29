c51feb90daa350e00d7bbdb54e556bc0ff4bb3d2
MAhsanOfficial <155831296+MAhsanOfficial@users.noreply.github.com>
2024-01-29 04:42:00 -0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,21 @@
+ 
+var number = prompt("Enter a number:");
+
+ 
+console.log("Multiplication Table for " + number + ":");
+for (var i = 1; i <= 10; i++) {
+    var result = number * i;
+    console.log(number + " * " + i + " = " + result);
+}
+ 
+var factorialNumber = 5;
+var factorialResult = 1;
+var j = 1;
+
+while (y <= factorialNumber) {
+    factorialResult *= j;
+    y++;
+}
+
+ 
+ 
